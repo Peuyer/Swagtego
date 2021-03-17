ba1fa94705857273bf8f2ac99decd70e30063031
jairaume <rasclejerome@gmail.com>
2021-03-17 15:07:52 +0100

    Update affichage.js

--- front/js/affichage.js
+++ front/js/affichage.js
@@ -1,6 +1,4 @@
 //Affiche le plateau et les pions 
-//let board = new Board();
-//const Board = require("../../back/models/board");
 
 class View{
 	constructor(game){
@@ -25,8 +23,8 @@ class View{
 			}
 		}
 	}
-	initBoard(board){
 
+	initBoard(board){
 		let html = '';
 		let index = 0;
 		for(let x=0; x<10; x++){
@@ -38,34 +36,7 @@ class View{
 				}
 				else if (board.board[x][y] == null){
 						html += '<td class="grass" data=',index,'>';
-						//html += "<td class='grass' data ='"\i\"'>"
 				}
-				/*else {
-					
-					if (board.getPawn(x,y) % 2 == 0){
-							if(board.board[x][y].isReturned == true){
-								
-								html += '<td class="pawnRed">';
-								text = board.board[x][y].pawn.toString(); 
-								html += text.slice(0,1);
-							}
-							else{
-								html += '<td class="pawnRedRecto">';
-							}
-					}
-					else if (board.getPawn(x,y) % 2 == 1){
-						if(board.board[x][y].isReturned == true){
-							html += '<td class="pawnBlue">';
-							text = board.board[x][y].pawn.toString(); 
-							html += text.slice(0,1);
-						}
-						else{
-							html += '<td class="pawnBlueRecto">';					
-						}
-					}
-				}
-				*/
-			
 				html += '</td>';
 			}
 			html += '</tr>';
@@ -85,43 +56,3 @@ class View{
 		return grid; 
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//board.placingPawns(0,0,3,2);
-//board.randomComposition(0);
-//board.randomComposition(1);
-/*board.placingPawns(1,0,8,3);
-board.moveList(0,3);
-board.move(0,3,0,8);
-board.moveList(9,3);
-board.move(9,3,9,4);
-*/
-
-
-
-//DisplayBoard(board);
